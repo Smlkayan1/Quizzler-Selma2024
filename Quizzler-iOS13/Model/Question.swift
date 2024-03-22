8bded2fd4afb01b340cd345269c59fc45e5187bf
Selma Kayan <selmakayan@gmail.com>
2024-03-21 22:15:01 -0400

    Last updated by Selma Kayan with enhancements involved

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,23 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Selma Kayan on 3/21/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    
+    init(q: String, a:String) {
+        text = q
+        answer = a
+    }
+}
+
+
+
